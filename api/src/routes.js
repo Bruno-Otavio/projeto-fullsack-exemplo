fd29b44bfa9a3c9079b938bf57bfc0a1db2cec33
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-07-30 11:26:56 -0300

    routes

---
+++ api/src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Middleware = require('./middleware/middleware');
+const Colaborador = require('./controllers/colaborador');
+const Os = require('./controllers/os');
+const Comentario = require('./controllers/comentario');
+
+router.post('/login', Colaborador.login);
+router.get('/colaborador', Middleware.validaAcesso, Colaborador.read);
+router.get('/colaborador/:matricula', Middleware.validaAcesso, Colaborador.read);
+
+router.get('/', (req, res) => {
+    return res.json('API respondendo');
+});
+
+module.exports = router;
