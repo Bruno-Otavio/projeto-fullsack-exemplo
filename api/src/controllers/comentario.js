f754d6b824af2cdec9b0267e72e58ecb637eebd5
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-07-31 10:38:41 -0300

    controllers crud

--- api/src/controllers/comentario.js
+++ api/src/controllers/comentario.js
@@ -1,4 +1,74 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-module.exports = {};
+const create = async (req, res) => {
+    try {
+        const { os, colaborador, comentario, data }  = req.body;
+        const coment = await prisma.comentario.create({
+            data: {
+                os: os,
+                colaborador: colaborador,
+                comentario: comentario,
+                data: data,
+            }
+        });
+        return res.status(201).json(coment);
+    } catch (error) {
+        return res.status(400).message({ message: error.message });
+    }
+}
+
+const read = async (req, res) => {
+    if (req.params.id !== undefined) {
+        const comentario = await prisma.comentario.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        res.status(200).json(comentario);
+    } else {
+        const comentarios = await prisma.comentario.findMany();
+        res.status(400).json(comentarios);
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const { id, os, colaborador, comentario, data }  = req.body;
+        const coment = await prisma.comentario.update({
+            where: {
+                id: id,
+            },
+            data: {
+                os: os,
+                colaborador: colaborador,
+                comentario: comentario,
+                data: data,
+            }
+        });
+        return res.status(202).json(coment);
+    } catch (error) {
+        return res.status(404).json({ message: "comentario não encontrado" })
+    }
+}
+
+const del = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const comentario = await prisma.comentario.delete({
+            where: {
+                id: id,
+            },
+        });
+        return res.status(204).json(comentario);
+    } catch (error) {
+        return res.status(404).json({ message: "comentario não encontrado" });
+    }
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    del
+};
--- api/src/controllers/os.js
+++ api/src/controllers/os.js
@@ -1,4 +1,75 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-module.exports = {};
+const create = async (req, res) => {
+    try {
+        const { descricao, colaborador, executor, abertura, encerramento }  = req.body;
+        const os = await prisma.os.create({
+            data: {
+                descricao: descricao,
+                colaborador: colaborador,
+                executor: executor,
+                abertura: abertura,
+                encerramento: encerramento,
+            }
+        });
+        return res.status(201).json(os);
+    } catch (error) {
+        return res.status(400).message({ message: error.message });
+    }
+}
+
+const read = async (req, res) => {
+    if (req.params.id !== undefined) {
+        const os = await prisma.os.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        res.status(200).json(os);
+    } else {
+        const oss = await prisma.os.findMany();
+        res.status(400).json(oss);
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const { id, descricao, colaborador, executor, abertura, encerramento }  = req.body;
+        const os = await prisma.os.update({
+            where: {
+                id: parseInt(id),
+            },
+            data: {
+                descricao: descricao,
+                colaborador: colaborador,
+                executor: executor,
+                abertura: abertura,
+                encerramento: encerramento,
+            }
+        });
+        return res.status(202).json(os);
+    } catch (error) {
+        return res.status(404).json({ message: "os não encontrado" })
+    }
+}
+
+const del = async (req, res) => {
+    try {
+        const os = await prisma.os.delete({
+            where: {
+                id: parseInt(req.params.id),
+            }
+        });
+        return res.status(204).json(os);
+    } catch (error) {
+        return res.status(404).json({ message: "os não encontrado" });
+    }
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    del
+};
